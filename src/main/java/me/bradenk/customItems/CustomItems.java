a7a15efce00973da303cef9987c79f5cc3cc98f0
bradenk04 <bradenk04@proton.me>
2026-04-11 21:14:06 -0700

    feat: Added ItemGiveCommand

--- src/main/java/me/bradenk/customItems/CustomItems.java
+++ src/main/java/me/bradenk/customItems/CustomItems.java
@@ -1,11 +1,16 @@
 package me.bradenk.customItems;
 
+import me.bradenk.customItems.command.CustomItemParameterType;
 import me.bradenk.customItems.command.ItemEditCommand;
+import me.bradenk.customItems.command.ItemGiveCommand;
 import me.bradenk.customItems.config.ConfigLoader;
+import me.bradenk.customItems.items.CustomItem;
 import org.bukkit.plugin.java.JavaPlugin;
 import revxrsal.commands.Lamp;
 import revxrsal.commands.bukkit.BukkitLamp;
 import revxrsal.commands.bukkit.actor.BukkitCommandActor;
+
+import java.util.Collections;
 
 public final class CustomItems extends JavaPlugin {
     public static CustomItems instance;
@@ -13,10 +18,19 @@ public final class CustomItems extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-        Lamp<BukkitCommandActor> lamp = BukkitLamp.builder(this).build();
-        lamp.register(new ItemEditCommand());
-
         ConfigLoader.load();
+
+        Lamp<BukkitCommandActor> lamp = BukkitLamp
+                .builder(this)
+                .parameterTypes(builder -> {
+                    builder.addParameterType(CustomItem.class, new CustomItemParameterType());
+                })
+                .suggestionProviders(providers -> {
+                    providers.addProvider(CustomItem.class, context -> Collections.list(ConfigLoader.customItems.keys()));
+                })
+                .build();
+        lamp.register(new ItemEditCommand());
+        lamp.register(new ItemGiveCommand());
     }
 
     @Override
